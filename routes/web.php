b8fea089ab260fc4cb7e69469953eeb4e424eb94
chikybambony <assault108@gmail.com>
2025-06-18 13:00:27 +0300

    add assets (frontend) layouts livewire

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Livewire\HomeComponent;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', \App\Livewire\HomeComponent::class)->name('home');
+Route::get('/category', \App\Livewire\Product\CategoryComponent::class)->name('category');
+Route::get('/product', \App\Livewire\Product\ProductComponent::class)->name('product');
